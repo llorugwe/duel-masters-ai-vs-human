23ee182c3f1021f5b7295498a17edcb57af2dd60
Lloyd <llorugwe@gmail.com>
2024-07-03 17:39:28 +0200

    Made changes to the front end directory

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SampleComponent from './components/SampleComponent'; // Import SampleComponent
+
+function App() {
+  return (
+    <div className="App">
+      <SampleComponent />  {/* Use SampleComponent */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/SampleComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SampleComponent() {
+  return (
+    <div>
+      <h1>Hello, this is a sample component!</h1>
+    </div>
+  );
+}
+
+export default SampleComponent;
