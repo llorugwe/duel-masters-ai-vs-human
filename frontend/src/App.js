4b31941774d46117369106ff3ebdb39593209ffa
Tinashe <tinaguhwa@gmail.com>
2024-07-11 13:34:36 +0200

    Corrected leaderboard issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CombinedAuth from './components/CombinedAuth';
 import GameInterface from './components/GameInterface';
-import ChooseOpponent from './components/ChooseOpponent';  // Add this import
+import ChooseOpponent from './components/ChooseOpponent';
+import Leaderboard from './components/Leaderboard'; // Import the Leaderboard component
 import './App.css';
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<CombinedAuth />} />
-          <Route path="/choose-opponent" element={<ChooseOpponent />} />  // Add this route
+          <Route path="/choose-opponent" element={<ChooseOpponent />} />
           <Route path="/game-interface" element={<GameInterface />} />
         </Routes>
+        <Leaderboard /> {/* Add the Leaderboard component here */}
       </div>
     </Router>
   );
