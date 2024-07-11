0a708bf3cbc00c66d86d8f41b9b276d16d0b8534
Tinashe <tinaguhwa@gmail.com>
2024-07-11 11:49:04 +0200

    Improved interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CombinedAuth from './components/CombinedAuth';
 import GameInterface from './components/GameInterface';
-import ChooseOpponent from './components/ChooseOpponent';
+import ChooseOpponent from './components/ChooseOpponent';  // Add this import
 import './App.css';
 
-const App = () => {
+function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<CombinedAuth />} />
-          <Route path="/choose-opponent" element={<ChooseOpponent />} />
+          <Route path="/choose-opponent" element={<ChooseOpponent />} />  // Add this route
           <Route path="/game-interface" element={<GameInterface />} />
         </Routes>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
